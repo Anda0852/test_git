c322bbb7b17ef4c3bb7f6a23413570f51c12e273
Anda0852 <anda0872702538@gmail.com>
2023-06-03 15:05:48 +0700

    fixed version 3

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const express = express()
 const PORT = 4000
 
-append.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`APi Listening on potr ${PORT}`)
 })
 
-append.get('/', (req, res) =>{
+app.get('/', (req, res) =>{
     res.send("This is API running ")
 })
 
-append.get('/about', (req,rs) =>{
+app.get('/about', (req,rs) =>{
     res.send('This is my about')
 })
 
